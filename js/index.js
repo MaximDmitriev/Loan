65bbf61a7393a4ec3a27d993fe58dd783c41179e
Maxim <maximv.dmitriev@gmail.com>
2018-11-02 21:37:22 +0300

    форма с маской телефона

--- js/index.js
+++ js/index.js
@@ -174,19 +174,18 @@ window.addEventListener('DOMContentLoaded', () => {
     statusInfo.style.paddingTop = 15 + "px";
 
 
-    function clearInput() {
-        for (let i=0; i < inputFormTime.length; i++) {
-            inputFormTime[i].value = '';
+    function sendFormTime(formName, inputname) {
+
+        function clearInput() {
+            for (let i=0; i < inputname.length; i++) {
+                inputname[i].value = '';
+            }
+            formName.removeChild(statusInfo);
         }
-        formTime.removeChild(statusInfo);
-    }
 
+        formName.appendChild(statusInfo);
 
-    function sendFormTime() {
-        formTime.appendChild(statusInfo);
-
-
-        let formData = new FormData(formTime),
+        let formData = new FormData(formName),
             request = new XMLHttpRequest();
 
         request.open('POST', '../server.php');
@@ -208,12 +207,12 @@ window.addEventListener('DOMContentLoaded', () => {
         setTimeout(clearInput, 2000);
     }
 
+
     inputFormTime[1].addEventListener('input', () => {
         let re = /[а-яё]/gi;
         inputFormTime[1].value = inputFormTime[1].value.replace(re, '');
 
     });
-
 
     inputFormTime[2].addEventListener('input', () => {
         let re = /[^\d\/\.]/g;
@@ -221,18 +220,58 @@ window.addEventListener('DOMContentLoaded', () => {
 
     });
 
-
     formTime.addEventListener('submit', (event) => {
         event.preventDefault();
 
         if (inputFormTime[0].value != '' && inputFormTime[1].value != '' && inputFormTime[2].value != '') {
-            sendFormTime();
-        }
-        
+            sendFormTime(formTime, inputFormTime);
+        }   
     });
 
+    // Form let us help
 
+    let formHelp = document.querySelector("#formHelp"),
+        inputFormHelp = formHelp.getElementsByTagName("input");
 
+        formHelp.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            if (inputFormHelp[0].value != '' && inputFormHelp[1].value != '' && inputFormHelp[2].value != '' && inputFormHelp[3].value != '') {
+            sendFormTime(formHelp, inputFormHelp);
+            }
+        });
+
+        inputFormHelp[2].addEventListener('input', () => {
+            let re = /[а-яё]/gi;
+            inputFormHelp[2].value = inputFormHelp[2].value.replace(re, '');
+    
+        });
+
+        function getMask(num) {
+            let start = "+1 (";
+            if (num.length > 0 && num.length <= 3 ){
+                inputFormHelp[3].value = start + num;
+            } else if (num.length > 3 && num.length <= 6) {
+                inputFormHelp[3].value = start + num.slice(0, 3) + ") " + num.slice(3);
+            } else if (num.length > 6 && num.length <= 10) {
+                inputFormHelp[3].value = start + num.slice(0, 3) + ") " + num.slice(3, 6) + " " + num.slice(6);
+            }
+            
+        }
+
+        inputFormHelp[3].addEventListener('input', (event) => {
+            event.preventDefault();
+            let re = /[\D]/g;
+            let number = inputFormHelp[3].value.replace(re, '');
+            
+            if (number.length > 1 && number.length <= 10) {
+                   number = number.slice(1);
+            } else if (number.length > 10){
+                number = number.slice(1,11);
+            }
+            getMask(number);
+           
+        });
 
 
 });
